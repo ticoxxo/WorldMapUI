5f6d42c8780ecc96c5e0b801f737a7a18617ef72
Ticoxxo <ticoxxo@gmail.com>
2023-10-03 10:22:37 -0700

    Initial Commit

---
+++ WorldMapUI/WorldMapUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldMapUIApp.swift
+//  WorldMapUI
+//
+//  Created by Alberto Almeida on 03/10/23.
+//
+
+import SwiftUI
+
+@main
+struct WorldMapUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
